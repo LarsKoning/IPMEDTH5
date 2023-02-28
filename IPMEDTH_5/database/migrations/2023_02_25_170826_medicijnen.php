351f29412ea0506f7e79d69467f9ebc3d99516dd
LarsKoning <koninglars04@gmail.com>
2023-02-28 12:23:03 +0100

    seeder voor: users, patient, medicijnen

--- IPMEDTH_5/database/migrations/2023_02_25_170826_medicijnen.php
+++ IPMEDTH_5/database/migrations/2023_02_25_170826_medicijnen.php
@@ -14,7 +14,7 @@ return new class extends Migration
             Schema::create('medicijnen', function (Blueprint $table) {
                 $table->id();
                 $table->string('naam');
-                $table->integer('dosis');
+                $table->integer('dosis (mg)');
             });
         }
 
--- IPMEDTH_5/database/seeders/DatabaseSeeder.php
+++ IPMEDTH_5/database/seeders/DatabaseSeeder.php
@@ -4,19 +4,97 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use DB;
+use Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('users')->insert(array(
+            [
+            'name' => 'sander',
+            'email' => 's1122866@student.hsleiden.nl',
+            'password' => Hash::make('password'),
+            ],
+            [
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('password'),
+            ],
+            [
+            'name' => 'Luca',
+            'email' => 's1136861@student.hsleiden.nl',
+            'password' => Hash::make('password'),
+            ],
+            [
+            'name' => 'Lars',
+            'email' => 's1136408@student.hsleiden.nl',
+            'password' => Hash::make('password'),
+            ],
+            [
+            'name' => 'Quinten',
+            'email' => 's1133415@student.hsleiden.nl',
+            'password' => Hash::make('password'),
+            ],
+        ));
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        DB::table('patient')->insert(array(
+            [
+            'voornaam' => 'jan',
+            'achternaam' => 'van der loop',
+            'adres' => 'Marco Pololaan 464',
+            ],
+            [
+            'voornaam' => 'Adriaan',
+            'achternaam' => 'monnik',
+            'password' => 'Kruisweg 55',
+            ],
+            [
+            'voornaam' => 'Anne',
+            'achternaam' => 'de Zwaan',
+            'password' => 'Kerkplein 6',
+            ],
+            [
+            'voornaam' => 'Mandy',
+            'achternaam' => 'Fansen',
+            'password' => 'Mercuriuslaan 16',
+            ],
+            [
+            'voornaam' => 'Kimberly',
+            'achternaam' => 'Corn',
+            'password' => 'Sikkelstraat 117',
+            ]
+        ));
+        DB::table('medicijnen')->insert(array(
+            [
+            'naam' => 'Diclofenac',
+            'dosis (mg)' => 50,
+            ],
+            [
+            'naam' => 'Amixicilline',
+            'dosis (mg)' => 500,
+            ],
+            [
+            'naam' => 'Omeprazol',
+            'dosis (mg)' => 10,
+            ],
+            [
+            'naam' => 'Doxycycline',
+            'dosis (mg)' => 100,
+            ],
+            [
+            'naam' => 'Ibuprofen',
+            'dosis (mg)' => 400,
+            ],
+            [
+            'naam' => 'Metoprolol',
+            'dosis (mg)' => 50,
+            ],
+            [
+            'naam' => 'Salbutamol',
+            'dosis (mg)' => 300,
+            ]
+        ));
     }
 }
