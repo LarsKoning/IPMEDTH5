1b122a17f8c697170a7fa46b59620972763aaea6
SanderICT <123902973+SanderICT@users.noreply.github.com>
2023-03-16 18:46:41 +0100

    controllers aangemaakt
    models, controllers, vieuw en css aangepast

---
+++ IPMEDTH_5/app/Http/Controllers/AppController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\patient;
+use App\Models\medicijn;
+use App\Models\inname;
+use App\Models\user;
+
+class AppController extends Controller
+{
+    function index(){
+        $patient = patient::first();
+        return view('index')->with('patient', $patient);
+    }
+}
